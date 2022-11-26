e1cbb38aa5bfaf619e98afbcb047f593fb6e84ba
WesleyMatthee <wrmatthee@gmail.com>
2022-11-26 02:46:51 +0000

    Pairing with Nawal

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
 
-module.exports = wordSearch
+    for (l of horizontalJoin) {
+        if (l.includes(word)) return true;
+    }
+
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+    } return false;
+
+};
+
+module.exports = wordSearch;
+
+const transpose = function(matrix) {
+    const newArr = [];
+    for (let c = 0; c < matrix[0].length; c++) {
+        const newRow = [];
+        for (let r = 0; r < matrix.length; r++) {
+            newRow.push(matrix[r][c]);
+        }
+        newArr.push(newRow);
+
+    }
+    return newArr;
+};
